a291ad35aafc5d51155ff725a69392f538f63b3a
Rafael Almeida <almeidarp.dev@gmail.com>
2025-01-14 09:11:12 -0300

    feat: create reverse words function

--- src/question5.js
+++ src/question5.js
@@ -1,1 +1,15 @@
-console.log("INICIANDO ARQUIVO 5")
+const word = "target sistemas";
+
+async function reverseWords(stringToReverse) {
+  let reversedString = "";
+
+  console.log("⚙️ Iniciando a reversão da palavra: ", word);
+  for (let i = stringToReverse.length - 1; i >= 0; i--) {
+    reversedString += stringToReverse[i];
+    console.log(stringToReverse[i]);
+  }
+  console.log("✅ Reversão concluída.");
+  console.log(`Palavra ao contrário: ${reversedString}`);
+}
+
+reverseWords(word);
