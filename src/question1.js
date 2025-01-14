17b87412252a48f8c0cb949ea554ee8c85621fde
Rafael Almeida <almeidarp.dev@gmail.com>
2025-01-14 09:06:19 -0300

    feat: add question1 sumloop function

--- src/question1.js
+++ src/question1.js
@@ -1,1 +1,24 @@
-console.log("INICIANDO ARQUIVO 1")
+const question1 = `Observe o trecho de código abaixo: 
+int INDICE = 13, SOMA = 0, K = 0; 
+Enquanto K < INDICE faça { K = K + 1; SOMA = SOMA + K; } 
+Imprimir(SOMA); 
+
+Ao final do processamento, qual será o valor da variável SOMA?
+`;
+console.log("❓ Pergunta: ", question1);
+
+function sumLoop() {
+  const index = 13;
+  let sum = 0;
+  let k = 0;
+
+  while (k < index) {
+    k = k + 1;
+    sum = sum + k;
+  }
+  return (result = `Ao final do processamento, o valor da variável SOMA é: ${sum}! ✅`);
+}
+
+sumLoop();
+console.log("\x1b[32m===========================================\x1b[0m");
+console.log(result);
